6164dedbb642d481d75cbfb5941d486ce4e69dee
Aman Fahimullah <faman@me.com>
2014-01-13 18:05:12 -0800

    date of birth project

---
+++ dateofbirth/born.scala
@@ -0,0 +1,10 @@
+object dateOfBirth {
+	def main(args: Array[String]){
+		print("Enter your name:")
+		val name = Console.readLine
+		print("Enter your DOB:")
+		val dob = Console.readLine
+		println(name + " was born on " + dob)
+	}
+
+}
